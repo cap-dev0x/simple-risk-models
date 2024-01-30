3b9b1f2245fdb8ce4e082c21a125332d1337a75b
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-01-30 06:47:41 -0800

    feat: simple rough draft of first model
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
-print("Test")
+#MODEL 1
+
+#Investment of funds
+invst = 1000.0
+
+#Average_time_before_investment
+avg_before = 12.4
+
+#Average_time_after_investment
+avg_after = 15
+
+investment_performence_score = (avg_before - avg_after ) * invst
+print(investment_performence_score)
