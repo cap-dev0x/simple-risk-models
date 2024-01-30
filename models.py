78f8437a5ef448f75eb16c3ffa52bca4d07ed6b8
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-01-30 07:20:27 -0800

    feat: rought draft for model 3
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- models.py
+++ models.py
@@ -29,3 +29,27 @@ investment_performance_score = hit_ratio * invst
 
 print("MODEL 2")
 print(investment_performance_score)
+
+#MODEL 3
+
+#Set of the times when official CVEs were first published
+discovery_wilds = [3,4,6]
+
+#Set of the times when CVEs were discovered on system
+discovery_system = [5,5,8]
+
+#CVE scores for the CVEs found 
+cve_scores  = [8.0,4.3,7,1]
+
+#Calculate datapoints for average
+impact_set = []
+for i in range(0,len(discovery_wilds)):
+    indv_impact = (discovery_system[i] - discovery_wilds[i]) * cve_scores[i]
+    impact_set.append(indv_impact)
+
+impact_score = sum(impact_set) / len(impact_set)
+
+investment_performance_score = impact_score * invst
+
+print("MODEL 3")
+print(investment_performance_score)
