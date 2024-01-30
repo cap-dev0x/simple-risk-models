46723b1b879258f47f3ad5008c6ad3223282a042
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-01-30 06:55:36 -0800

    feat: added formula for second model
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- models.py
+++ models.py
@@ -9,5 +9,23 @@ avg_before = 12.4
 #Average_time_after_investment
 avg_after = 15
 
-investment_performence_score = (avg_before - avg_after ) * invst
-print(investment_performence_score)
+investment_performance_score = (avg_before - avg_after ) * invst
+
+print("MODEL 1")
+print(investment_performance_score)
+
+#MODEL 2
+
+#Total number of emails phished in a set period
+phish_total = 409587
+
+#Total number of employee responses to phishing attempts (hits)
+hit_total = 2
+
+#Hit Ratio of Attacks
+hit_ratio = hit_total/phish_total
+
+investment_performance_score = hit_ratio * invst
+
+print("MODEL 2")
+print(investment_performance_score)
