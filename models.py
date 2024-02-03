d2b8a6ca2634a6a26852885bb45a1fffad73916a
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-02-03 09:41:39 -0800

    feat(models.py): created object for model 1
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- models.py
+++ models.py
@@ -3,34 +3,61 @@
 
 #Function Library
 
-#Simple function to get the input of the users, this input is the dollar
-#amount that the company has invested, will be standardized to a float for 
-#all calculations - will NOT round to hundreds place 
-def get_number_input():
-    while True:
-        try:
-            user_input = input("Enter the numerical dollar value of  investment: ")
-            number = float(user_input)  # Standardize to float for all calcs
-            return number  # Return the float
-        except ValueError:
-            print("Invalid input. Please enter a valid number (int or float).")
 
-#Investment of funds
-invst = get_number_input()
+#MODEL 1 
 
-#Average_time_before_investment
-avg_before = 12.4
+class Model_1:
+    def __init__(self):
+        self.investment = self.get_number_input()
+        self.avg_before = self.get_number_before()
+        self.avg_after = self.get_number_after()
+        self.investment_performance_score = (self.avg_before - 
+                                            self.avg_after ) * self.investment
 
-#Average_time_after_investment
-avg_after = 15
+    #Simple function to get the input of the users, this input is the dollar
+    #amount that the company has invested, will be standardized to a float for 
+    #all calculations - will NOT round to hundreds place 
+    def get_number_input(self):
+        while True:
+            try:
+                user_input = input("Enter the numerical dollar value of  investment: ")
+                number = float(user_input)  # Standardize to float for all calcs
+                return number  # Return the float
+            except ValueError:
+                print("Invalid input. Please enter a valid number (int or float).")
 
-investment_performance_score = (avg_before - avg_after ) * invst
+    #Simple function to get the input of the users, this input is the dollar
+    #amount that the company has invested, will be standardized to a float for 
+    #all calculations - will NOT round to hundreds place 
+    def get_number_before(self):
+        while True:
+            try:
+                user_input = input("Enter the average fix time before investment: ")
+                number = float(user_input)  # Standardize to float for all calcs
+                return number  # Return the float
+            except ValueError:
+                print("Invalid input. Please enter a valid number (int or float).")
 
-print("MODEL 1")
-print(investment_performance_score)
+    #Simple function to get the input of the users, this input is the dollar
+    #amount that the company has invested, will be standardized to a float for 
+    #all calculations - will NOT round to hundreds place 
+    def get_number_after(self):
+        while True:
+            try:
+                user_input = input("Enter the average fix time after investment: ")
+                number = float(user_input)  # Standardize to float for all calcs
+                return number  # Return the float
+            except ValueError:
+                print("Invalid input. Please enter a valid number (int or float).")
+
+    def __str__(self):
+        return f"Investment: {self.investment}, Avg Before: {self.avg_before}, Avg After: {self.avg_after}"
+
+test = Model_1()
+print(test.investment_performance_score)
 
 #MODEL 2
-
+invst = 1000.0
 #Total number of emails phished in a set period
 phish_total = 409587
 
