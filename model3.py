0660fbb187cbb483b4b21ad6ab9492aa5dc1fa7d
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-02-27 10:29:54 -0800

    feat(model3.pt): getters function now working
    Model 3 is now fully functioning.
    
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- model3.py
+++ model3.py
@@ -10,22 +10,26 @@ class Model_3:
         self.impact_score = False #currently set to false for troubleshooting
         self.investment_performance_score = False #same set as impact_score
 
+    #Helper function for get_arrays_inputs(), allows for a narrowing of 
+    #get_arrays_input's functionality
+    def get_arrays_size(self):
+        while True:
+            try:
+                user_input = int(input("Enter the total number of iterations: "))
+                return user_input
+            except:
+                print("Invalid input, please enter a whole number")
+
     #User sets the number of iterations, populates the user input into the 
     #the arrays of the object
     #TODO add input validation for each of the input functions in the foo
-    def get_inputs(self):
-        total_rounds = 0
-        while total_rounds == 0:
-            try:
-                user_input = input("Enter the total number of iterations: ")
-                total_rounds = user_input
-            except:
-                print("Invalid input, please enter a whole number")
+    def get_arrays_inputs(self):
+        total_rounds = self.get_arrays_size()
         
         for item in range(0, total_rounds):
-            self.discovery_wilds[item] = input("Enter the time of CVE publication")
-            self.discovery_systems[item] = input("Enter the time of discovery on system")
-            self.cve_scores[item] = input("Enter the CVE score for the CVE")
+            self.discovery_wilds.append(int(input("Enter the time of CVE publication")))
+            self.discovery_systems.append(int(input("Enter the time of discovery on system")))
+            self.cve_scores.append(int(input("Enter the CVE score for the CVE")))
             
     #Simple function to get the input of the users, this input is the dollar
     #amount that the company has invested, will be standardized to a float for 
@@ -44,17 +48,35 @@ class Model_3:
     #array
     def calc_impact_set(self):
         for i in range(0,len(self.discovery_wilds)):
-            indv_impact = (self.discovery_system[i] - self.discovery_wilds[i]) * self.cve_scores[i]
+            indv_impact = (self.discovery_systems[i] - self.discovery_wilds[i]) * self.cve_scores[i]
             self.impact_set.append(indv_impact)
 
-    #
+    #Calculates the impact score by finding the average exposure time of the 
+    #CVEs
     def calc_impact_score(self):
-        self.impact_score = sum(self.calc_impact_set) / len(self.impact_set)
+        self.impact_score = sum(self.impact_set) / len(self.impact_set)
 
     #Higher in value the investment score, the more impact the investment 
     #has had. 
     def calc_investment_performance_score(self):
         self.investment_performance_score = self.impact_score * self.investment
+
+    #Function to run the model
+    def run_model_3(self):
+        self.get_arrays_inputs()
+        self.calc_impact_set()
+        self.calc_impact_score()
+        self.calc_investment_performance_score()
+
+        print("###Model 3 Analysis###")
+        print(f"Initial Investment: {self.investment}")
+        print(f"Total CVEs Found: {len(self.discovery_systems)}")
+        print(f"Average Discovery Time: {self.impact_score}")
+        print(f"Investment Performance Score: {self.investment_performance_score}")
+
+test_3 = Model_3()
+test_3.run_model_3()
+
 
 
 
